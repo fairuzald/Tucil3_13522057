7586394d01c227b16a225d6a4e539c8e6ad447f7
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-05-04 13:43:07 +0700

    feat: create respopnsive gui

--- src/packages/frontend/app/layout.tsx
+++ src/packages/frontend/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <Toaster />
+        {children}
+      </body>
     </html>
   );
 }
--- src/packages/frontend/app/page.tsx
+++ src/packages/frontend/app/page.tsx
@@ -1,130 +1,92 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
 
-async function getTestContent() {
-  const response = await fetch("http://localhost:8080/greeting");
-  const data = await response.json();
-  return data;
-}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { FormEventHandler, useState } from "react";
+import toast from "react-hot-toast";
 
-export default  function Home() {
-  const [content, setContent] = useState(null);
-  useEffect(() => {
-    getTestContent().then((data) => setContent(data));
+
+export default function Home() {
+  const [algorithm, setAlgorithm] = useState("ucs");
+  const [startWord, setStartWord] = useState("");
+  const [endWord, setEndWord] = useState("");
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!startWord || !endWord || startWord.length !== endWord.length || !algorithm) {
+      if (!startWord) toast.error("Start word is required");
+      if (!endWord) toast.error("End word is required");
+      if (startWord.length !== endWord.length) toast.error("Start word and end word must have the same length");
+      if (!algorithm) toast.error("Algorithm is required");
+      return;
+    }
+
+    console.log(startWord, endWord, algorithm);
   }
-  , []);
-
-  console.log(content);
-
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen px-8 lg:px-24 py-14 w-full relative z-10 p-8 md:p-10 lg:p-16 xl:p-20 2xl:p-24 flex flex-col items-center justify-center gap-10 lg:gap-12">
+      {/* Video background */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+        autoPlay
+        loop
+        muted
+      >
+        <source src={"bg3.mp4"} type="video/mp4" />
+      </video>
+
+      {/* Title Section */}
+      <section className="space-y-3 lg:space-y-5">
+        <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold text-theme-5">Word Ladder Solver</h1>
+        <h2 className="text-center text-2xl md:text-3xl lg:text-4xl font-bold text-theme-5">This solver using 3 algorithm as option</h2>
+      </section>
+
+      <form className="flex flex-col gap-7 lg:gap-10 items-center" onSubmit={onSubmit}>
+        {/* Input */}
+        <div className="flex gap-6 lg:gap-14 flex-col lg:flex-row">
+          {/* Start words input  */}
+          <div className="space-y-1.5 lg:space-y-3">
+            <h3 className="text-center text-theme-4 font-medium text-xl lg:text-2xl">Start Words</h3>
+            <Input type="text" placeholder="Start word" value={startWord} onChange={(e) => setStartWord(e.target.value)} />
+          </div>
+          {/* End word input */}
+          <div className="space-y-1.5 lg:space-y-3">
+            <h3 className="text-center text-theme-4 font-medium text-xl lg:text-2xl">End Words</h3>
+            <Input type="text" placeholder="End word" value={endWord} onChange={(e) => setEndWord(e.target.value)} />
+          </div>
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {/* Radio Algorithm */}
+        <div className="space-y-5">
+          <h3 className="text-center text-theme-4 text-xl lg:text-2xl font-medium">Select Algorithm</h3>
+          <RadioGroup className="flex lg:items-center max-md:flex-col justify-center gap-4 lg:gap-10" defaultValue="ucs" onValueChange={(selected) => {
+            setAlgorithm(selected);
+          }}>
+            {/* UCS */}
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="ucs" id="ucs" />
+              <Label htmlFor="ucs">Uniform Cost Seach</Label>
+            </div>
+            {/* GBFS */}
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="gbfs" id="gbfs" />
+              <Label htmlFor="gbfs">Greedy Best First Search</Label>
+            </div>
+            {/* Astar */}
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="astar" id="astar" />
+              <Label htmlFor="astar">A*</Label>
+            </div>
+          </RadioGroup>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        {/* Submit */}
+        <Button type="submit" variant="default" size="default">Solve</Button>
+      </form>
     </main>
   );
 }
